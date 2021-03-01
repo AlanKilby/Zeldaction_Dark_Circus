4cad8916fadc78de4034b73e21bbb348327de656
Benji <f.malagugini@orange.fr>
2021-03-01 03:06:32 +0100

    + patroller

--- Assets/BEN/Scripts/MoveTo.cs
+++ Assets/BEN/Scripts/MoveTo.cs
@@ -1,15 +1,36 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+// make this called by AI when player is in range, and override speed with custom one (preset stored in a ScriptableObject)
 public class MoveTo : MonoBehaviour
 {
     public Transform goal;
+    public bool useMouse = false; 
+    private NavMeshAgent agent;
 
     void Start()
     {
-        // make this called by AI when player is in range, and override speed with custom one (preset stored in a ScriptableObject)
-        NavMeshAgent agent = GetComponent<NavMeshAgent>(); 
-        agent.destination = goal.position; 
+        agent = GetComponent<NavMeshAgent>();
+        if (!useMouse)
+        {
+            agent.destination = goal.position;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && useMouse)
+        {
+            TryGetMouseClickPosition(); 
+        }
+    } 
+
+    void TryGetMouseClickPosition()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+        {
+            agent.destination = hit.point;
+        }
     }
 }
 
