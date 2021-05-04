2c2306fde26792cda05ea5a3f6b8bed3e042e15e
Charles <c.martinelli@rubika-edu.com>
2021-05-04 10:28:14 +0200

    Pause added

---
+++ Assets/CHARLES/Scripts/CHM_Pause.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHM_Pause : MonoBehaviour
+{
+    public GameObject pauseObject;
+
+    private bool gameIsPaused;
+
+    private void Start()
+    {
+        gameIsPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameIsPaused = !gameIsPaused;
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        if (gameIsPaused)
+        {
+            pauseObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
