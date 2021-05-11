3d1406139e5c3cdcafc5c41e05d66d8c2d0ab3e6
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-05-11 15:37:53 +0200

    Hotfix
    Optimized scripts for Cotton Candy and Slowing Zone.
    WiP: Loot drop script.

---
+++ Assets/AK/AK_Traps/AK_LootDrop.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AK_LootDrop : MonoBehaviour
+{
+    public GameObject[] itemsToDrop;
+
+    public float[] dropPercentage;
+
+    public void LootDrop()
+    {
+
+    }
+}
