f3ebec9f7c9be94ca4d1d809b17db5cc66b897ae
Ulric <ulric.debaert@outlook.fr>
2021-05-16 10:47:38 +0200

    HealthPointUI+DungeonPrefab

---
+++ Assets/UD/UD_Script/UD_UIHealthPoint.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UD_UIHealthPoint : MonoBehaviour
+{
+    List<GameObject> HealthPointSprites;
+
+    Health He;
+
+    void Start()
+    {
+        He = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < HealthPointSprites.Count; i++)
+        {
+            //if (He.CurrentValue < i)
+            //{
+            //    HealthPointSprites[i].SetActive(false);
+            //}
+            //else
+            //{
+            //    HealthPointSprites[i].SetActive(true);
+            //}
+        }
+    }
+}
