4ef2834519c5a263321d13f0a5c70e35ca2ce47c
Ulric <ulric.debaert@outlook.fr>
2021-05-20 10:44:58 +0200

    MenuOptionPrefab

---
+++ Assets/UD/UD_Script/UD_OptionMenu.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public enum Difficulty { Easy, Normal, Hard}
+public class UD_OptionMenu : MonoBehaviour
+{
+    public Dropdown resolutionDropdown;
+    public Slider volumeSlider;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    private void Update()
+    {
+        //static.volume = volumeSlider.value;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetDifficulty(int DifficultyIndex)
+    {
+        if (DifficultyIndex == 0)
+        {
+            SetEasy();
+        }
+        if (DifficultyIndex == 1)
+        {
+            SetNormal();
+        }
+        if (DifficultyIndex == 2)
+        {
+            SetHard();
+        }
+    }
+
+
+    public void SetEasy()
+    {
+        print("easy");
+        //currentDifficulty = Difficulty.Easy;
+    }
+    
+    public void SetNormal()
+    {
+        print("normal");
+        //currentDifficulty = Difficulty.Normal;
+    }
+    
+    public void SetHard()
+    {
+        print("hard");
+        //currentDifficulty = Difficulty.Hard;
+    }
+}
