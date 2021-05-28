6fae3db55d244c3c23339942b4a12962ee7c3567
Alan Kilby <alan.kilby.cerquera@gmail.com>
2021-05-28 12:06:14 +0200

    Anims + Stop movement + Potion/Heart fix

--- Assets/AK/AK_Props/AK_Potion.cs
+++ Assets/AK/AK_Props/AK_Potion.cs
@@ -6,20 +6,22 @@ public class AK_Potion : MonoBehaviour
 {
     Animator potionAnim;
     BoxCollider potionCollider;
-
+    AK_PlayerPotionManager potionManager;
     public float destroyTime = 4;
     private void Start()
     {
         potionAnim = GetComponent<Animator>();
         potionCollider = GetComponent<BoxCollider>();
+        potionManager = GameObject.FindGameObjectWithTag("Player").GetComponent<AK_PlayerPotionManager>();
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Player")){
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player") && potionManager.potionQuantity < potionManager.maxPotionQuantity)
+        {
 
-            Debug.Log("PlayerCollisionPotion");
-            collision.GetComponent<AK_PlayerPotionManager>().potionQuantity++;
+            //Debug.Log("PlayerCollisionPotion");
+            potionManager.potionQuantity++;
             potionAnim.Play("collected");
             potionCollider.enabled = false;
             Destroy(gameObject,destroyTime);      
