ca7b1b98dd3dd712a10e472c9892e399b7676507
Ulric <ulric.debaert@outlook.fr>
2021-05-29 13:49:49 +0200

    Game Over Screen Inte + AK_Props FIX

--- Assets/AK/AK_Player/Boomerang.cs
+++ Assets/AK/AK_Player/Boomerang.cs
@@ -112,7 +112,7 @@ public class Boomerang : MonoBehaviour
             Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
             float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0, rot, 0);
-            comebackTimer++;
+            //comebackTimer++;
         }
     }
  
