ea54f720a035991525d94a75b4956d7b5ec880c1
Ulric <ulric.debaert@outlook.fr>
2021-05-31 13:19:19 +0200

    CutoutScript_FIX

--- Assets/YANN/YMI_SCRIPTS/YMI_CutOutDetection.cs
+++ Assets/YANN/YMI_SCRIPTS/YMI_CutOutDetection.cs
@@ -14,6 +14,16 @@ public class YMI_CutOutDetection : MonoBehaviour
 
     private Camera mainCamera;
 
+    //Ajout Ulric
+    public RaycastHit[] hitObjects;
+    Vector2 cutoutPos;
+    public float cutoutSize;
+    public float cutoutSmoothness;
+    public Transform playerPos;
+    bool playerInSight;
+    public List<RaycastHit> hitObjectsMemory = new List<RaycastHit>();
+    //
+
     //Getting the camera to avoid error 
     private void Awake()
     {
@@ -23,7 +33,7 @@ public class YMI_CutOutDetection : MonoBehaviour
     void Update()
     {
         //Not sure
-        Vector2 cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
+        cutoutPos = mainCamera.WorldToViewportPoint(targetObject.position);
         cutoutPos.y /= (Screen.width / Screen.height);
 
         //not sure²
@@ -31,19 +41,77 @@ public class YMI_CutOutDetection : MonoBehaviour
         //Debug.Log(offset.magnitude);
         //Creating all the rays going from the transform of the targeted object to the wall ? And extracting the magnitude value
         // to check when where to apply the sahder 
-        RaycastHit[] hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, Wall);
+        hitObjects = Physics.RaycastAll(transform.position, offset, offset.magnitude, Wall);
 
         for (int i = 0; i < hitObjects.Length; i++)
         {
             Material[] materials = hitObjects[i].transform.GetComponent<Renderer>().materials;
 
-            //for (int n = 0; n < materials.Length; n++)
-            //{
-                
-               // materials[n].SetVector("_CutoutPos", cutoutPos);
-               // materials[n].SetFloat("_CutoutSize", 0.1f);
-               // materials[n].SetFloat("_CutoutSmoothness", 0.05f);
-            //}
+            for (int n = 0; n < materials.Length; n++)
+            {
+
+                materials[n].SetVector("_CutoutPos", cutoutPos);
+                materials[n].SetFloat("_CutoutSize", cutoutSize);
+                materials[n].SetFloat("_CutoutSmoothness", cutoutSmoothness);
+            }
+            if (CheckName(hitObjects[i]))
+            {
+                hitObjectsMemory.Add(hitObjects[i]);
+            }
+        }
+        Invoke("CheckWallInSight", 0.2f); 
+        
+        if (playerInSight)
+        {
+            ClearList();
+        }
+    }
+
+    void CheckWallInSight()
+    {
+        if (Physics.Linecast(transform.position, playerPos.position, Wall))
+        {
+            playerInSight = false;
+        }
+        else { playerInSight = true; }
+
+        
+    }
+
+    void ClearList()
+    {
+        for (int i = 0; i < hitObjectsMemory.Count; i++)
+        {
+            Material[] materials = hitObjectsMemory[i].transform.GetComponent<Renderer>().materials;
+
+            for (int n = 0; n < materials.Length; n++)
+            {
+
+                materials[n].SetVector("_CutoutPos", cutoutPos);
+                materials[n].SetFloat("_CutoutSize", 0.0f);
+                materials[n].SetFloat("_CutoutSmoothness", 0.0f);
+            }
+        }
+        hitObjectsMemory.Clear();
+    }
+
+    bool CheckName(RaycastHit hitObject)
+    {
+        foreach(RaycastHit n in hitObjectsMemory)
+        {
+            if (hitObject.collider.name == n.collider.name)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (playerPos != null)
+        {
+            Gizmos.DrawLine(transform.position, playerPos.position);
         }
     }
 }
